a06734bf45692a9a24b39e12f59c64b1b445c4be
Hugo Vargas <hugale03@gmail.com>
2020-08-02 02:54:50 -0400

    Se completa el archivo webcam.py para logra hacer funcionar con la camara web, aplicando escala de grises.

--- webcam.py
+++ webcam.py
@@ -1,15 +1,29 @@
-import cv2 as cv
+#pip3 install opencv-python
+#pip3 install opencv-contrib-python
 
-cap = cv.VideoCapture(0)
+import numpy as np
+import cv2
 
-while True:
-	ret, img = cap.read()
-	img = cv.flip(img, 1)
-	cv.imshow("Hola mundo", img)
+cap = cv2.VideoCapture(0)
 
-	k = cv.waitKey(30) & 0xff
-	if k == 27:
-		break
+if cap.read() == False:
+    cap.open()
 
+if not cap.isOpened():
+    print('Cannot open camera')
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    #import pdb; pdb.set_trace()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
